8ef7f586942a1f23c0ab59fa27b0d9a643926d64
Aldair Alfonso Gonzalez Amaya <aldair.gonzalez@unipamplona.edu.co>
2023-04-21 11:47:36 -0500

    Clase 8 --> Importando datos

---
+++ LinqQueries.cs
@@ -0,0 +1,24 @@
+using curso_linq.Entidades;
+
+namespace curso_linq
+{
+    public class LinqQueries
+    {
+        private List<Book> librosCollection = new List<Book>();
+
+        public LinqQueries()
+        {
+            using (StreamReader reader = new StreamReader("books.json"))
+            {
+                string json = reader.ReadToEnd();
+                this.librosCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            }
+        }
+
+
+        public IEnumerable<Book> TodaLaColeccion()
+        {
+            return librosCollection;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using curso_linq;
+using curso_linq.Entidades;
+
+LinqQueries queris = new LinqQueries();
+
+ImprimirValores(queris.TodaLaColeccion());
+
+void ImprimirValores(IEnumerable<Book> listaLibros)
+{
+    Console.WriteLine("{0,-60} {1,15} {2,15}\n", "Titulo", "# PAginas", "Fecha Publicacion");
+
+    foreach(var item in listaLibros)
+    {
+        Console.WriteLine("{0,-60} {1,15} {2,15}", item.Title,item.PageCount,item.PublishedDate.ToShortDateString());
+    }
+}
+
